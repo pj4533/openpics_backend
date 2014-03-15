5070194daa4dddc65ee04fcb6c39726da39f3d20
PJ Gray <pj@evertrue.com>
2014-03-15 18:09:52 -0400

    Updating import script

--- scripts/import_from_kinvey.rb
+++ scripts/import_from_kinvey.rb
@@ -27,45 +27,51 @@ images.each do |image|
 	image['date'] = Time.parse(raw_date)
 end
 
+image_urls = []
+
 images.each do |image|
-	query = "INSERT INTO images (date,image_url,image_provider_specific,image_provider_type,image_title,image_width,image_height) VALUES (\'#{image['date']}\',"
+	if not image_urls.include?(image['imageUrl'])
+		image_urls << image['imageUrl']
+		
+		query = "INSERT INTO images (date,image_url,image_provider_specific,image_provider_type,image_title,image_width,image_height) VALUES (\'#{image['date']}\',"
 
-	if image['imageUrl'] != ''
-		image_url = image['imageUrl']
-		query = "#{query}\'#{image_url}\'," 
-	end
-	if image['providerSpecific'] != ''
-		image_provider_specific = image['providerSpecific'].to_json
-		query = "#{query}\'#{c.escape_string(image_provider_specific)}\'," 
-	end
-	if image['providerType'] != ''
-		image_provider_type = image['providerType']
-		query = "#{query}\'#{c.escape_string(image_provider_type)}\'," 
-	end
-	if image['title'] == nil
-		query = "#{query}\'\'" 
-	else
-		image_title = image['title']
-		query = "#{query}\'#{c.escape_string(image_title)}\'" 
-	end
-	if image['width'] == nil
-		query = "#{query},null" 
-	else
-		image_width = image['width']		
-		query = "#{query},#{image_width}" 
-	end
-	if image['height'] == nil
-		query = "#{query},null" 
-	else
-		image_height = image['height']
-		query = "#{query},#{image_height}" 
-	end
+		if image['imageUrl'] != ''
+			image_url = image['imageUrl']
+			query = "#{query}\'#{image_url}\'," 
+		end
+		if image['providerSpecific'] != ''
+			image_provider_specific = image['providerSpecific'].to_json
+			query = "#{query}\'#{c.escape_string(image_provider_specific)}\'," 
+		end
+		if image['providerType'] != ''
+			image_provider_type = image['providerType']
+			query = "#{query}\'#{c.escape_string(image_provider_type)}\'," 
+		end
+		if image['title'] == nil
+			query = "#{query}\'\'" 
+		else
+			image_title = image['title']
+			query = "#{query}\'#{c.escape_string(image_title)}\'" 
+		end
+		if image['width'] == nil
+			query = "#{query},null" 
+		else
+			image_width = image['width']		
+			query = "#{query},#{image_width}" 
+		end
+		if image['height'] == nil
+			query = "#{query},null" 
+		else
+			image_height = image['height']
+			query = "#{query},#{image_height}" 
+		end
 
-	query = "#{query})"
+		query = "#{query})"
 
-	c.exec(query)
+		c.exec(query)
 
-	p "Imported #{image['title']}"
+		p "Imported #{image['title']}"
+	end
 end
 
 c.close
